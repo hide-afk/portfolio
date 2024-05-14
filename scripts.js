a8f9c4aaa3845513badbb3f1483116b3a5ab7f45
hide-afk <abadplatasmarc2007@gmail.com>
2024-05-14 19:34:47 +0200

    Certificates added

--- scripts.js
+++ scripts.js
@@ -1,15 +1,29 @@
 function mostrarAcademica() {
     var SeccionAcademica = document.querySelector("#SeccionAcademica");
     var SeccionLaboral = document.querySelector("#SeccionLaboral");
+    var SeccionCursos = document.querySelector("#SeccionCursos");
 
     SeccionAcademica.style.display = "block";
     SeccionLaboral.style.display = "none";
+    SeccionCursos.style.display = "none";
 }
 
 function mostrarLaboral() {
     var SeccionAcademica = document.querySelector("#SeccionAcademica");
     var SeccionLaboral = document.querySelector("#SeccionLaboral");
+    var SeccionCursos = document.querySelector("#SeccionCursos");
 
     SeccionLaboral.style.display = "block";
     SeccionAcademica.style.display = "none";
+    SeccionCursos.style.display = "none";
+}
+
+function mostrarCursos() {
+    var SeccionAcademica = document.querySelector("#SeccionAcademica");
+    var SeccionLaboral = document.querySelector("#SeccionLaboral");
+    var SeccionCursos = document.querySelector("#SeccionCursos");
+
+    SeccionLaboral.style.display = "none";
+    SeccionAcademica.style.display = "none";
+    SeccionCursos.style.display = "block";
 }
